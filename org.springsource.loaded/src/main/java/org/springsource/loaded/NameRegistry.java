feaf6b4a8c4a77b173e8df95854db7adfb6bd457
Andy Clement <andrew.clement@gmail.com>
2013-05-29 07:59:47 -0700

    Fixes #8: ensure array is always large enough

--- org.springsource.loaded/src/main/java/org/springsource/loaded/NameRegistry.java
+++ org.springsource.loaded/src/main/java/org/springsource/loaded/NameRegistry.java
@@ -80,7 +80,7 @@ public class NameRegistry {
 		int id = getIdFor(slashedClassName);
 		if (id == -1) {
 			id = nextTypeId++;
-			if (id >= allocatedIds.length) {
+			if (nextTypeId >= allocatedIds.length) {
 				size = size + 10;
 				// need to make more room
 				String[] newAllocatedIds = new String[size];
