10b95ef8c12f361051a29ad73887a7a335d50ab3
Andy Clement <aclement@gopivotal.com>
2014-02-03 20:27:44 -0800

    organize imports

--- springloaded/src/main/java/org/springsource/loaded/Utils.java
+++ springloaded/src/main/java/org/springsource/loaded/Utils.java
@@ -19,7 +19,6 @@ import java.io.BufferedInputStream;
 import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
